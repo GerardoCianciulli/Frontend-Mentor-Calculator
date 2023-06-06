807dec04eb1cf990431f1df8b2a8f854301fe221
Gerardo Cianciulli <gerardo.cianciulli@gmail.com>
2023-06-06 11:57:11 -0400

    basic game logic

--- rock-paper-scissors-lizard-spock.js
+++ rock-paper-scissors-lizard-spock.js
@@ -1,4 +1,14 @@
+const RULES = {
+    rock: ["lizard", "scissors"],
+    paper: ["rock", "spock"],
+    scissors: ["lizard", "paper"],
+    lizard: ["paper", "spock"],
+    spock: ["rock", "scissors"]
+}
+
 $(document).ready(function(){
+    var score = 0;
+
     $("#rules-btn").click(function(){
         $("#rules").removeClass("hide").addClass("show-flex")
     });
@@ -6,4 +16,18 @@ $(document).ready(function(){
     $("#rules .close").click(function(){
         $("#rules").addClass("hide").removeClass("show-flex")
     });
+
+    $("#gestures button").click(function(e){
+        console.log(e.currentTarget.id);
+        //show user selection
+        //add a delay
+        //show computer's selection 
+        var computerSelection = Math.round(Math.random() * Date.now() % 5)
+        console.log(Object.keys(RULES)[computerSelection])
+        if (RULES[e.currentTarget.id].includes(Object.keys(RULES)[computerSelection])) {
+            console.log("you win")
+            score ++;
+            $("#score span").text(score);
+        }
+    })
 });
